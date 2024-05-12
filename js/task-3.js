84dcf27ecab55235ac89eab78b28d2f3c5fbfe5f
Anastasiia Savinova Occhiminuti <anastesie@hotmail.com>
2024-05-12 23:45:37 +0200

    update

--- js/task-3.js
+++ js/task-3.js
@@ -6,26 +6,24 @@ class StringBuilder {
     constructor(value) {
         this.#value = value;
     }
-
+    
     getValue() {
         return this.#value;
     }
-
     padStart(str) {
-               this.#value = str + this.#value;
-    }
-    
+        //this.#value = str + this.#value;
+        this.#value = this.#value.padStart(this.#value.length + str.length, str);
+    }    
     padEnd(str) {
-
-        this.#value = this.#value + str;
-
+        // this.#value = this.#value + str;
+          this.#value = this.#value.padEnd(this.#value.length + str.length, str)
     }
-
     padBoth(str) {
-        
           this.#value = str + this.#value + str;
     }
 } 
+
+
 
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
